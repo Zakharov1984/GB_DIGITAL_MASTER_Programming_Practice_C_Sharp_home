788f9e823e21e69b8ec11680ed20b0afcaa8fe65
Zakharov <zakharov.products@gmail.com>
2022-09-19 19:37:41 +0300

    Third_task_completed

---
+++ Part_1/Task_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Пожалуйста, введите число: ");
+int numberValue = Convert.ToInt32( Console.ReadLine() );
+
+if (numberValue % 2 == 0) {
+    Console.Write("Введенное число является четным!");
+}else {
+    Console.Write("Введенное число является нечетным!");
+}
