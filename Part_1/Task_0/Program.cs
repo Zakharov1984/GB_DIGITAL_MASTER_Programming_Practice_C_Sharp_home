cd4bbe352c3db4ffea81085a154ecb8470c795c0
Zakharov <zakharov.products@gmail.com>
2022-09-19 18:07:56 +0300

    First_task_completed

---
+++ Part_1/Task_0/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Пожалуйста, введите первое число для сравнения: ");
+int number1 = Convert.ToInt32( Console.ReadLine() );
+Console.Write("Пожалуйста, введите второе число для сравнения: ");
+int number2 = Convert.ToInt32( Console.ReadLine() );
+
+if (number1 < number2) {
+    Console.WriteLine($"В этом случае число {number1} меньше, чем число {number2}");
+}else {
+    Console.WriteLine($"В этом случае число {number2} меньше, чем число {number1}");
+}
