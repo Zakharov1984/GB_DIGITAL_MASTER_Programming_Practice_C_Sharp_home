bce9fb21b407d55a53b501ff1a3cd222bdb805eb
Zakharov <zakharov.products@gmail.com>
2022-09-19 19:53:17 +0300

    Fourth_task_completed

---
+++ Part_1/Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Пожалуйста, введите число: ");
+int numberValue = Convert.ToInt32( Console.ReadLine() );
+string StringValue = "";
+
+for (int i = 1; i <= numberValue; i++) {
+    if (i % 2 == 0) {
+        StringValue = StringValue + " " + i;
+    }
+}
+
+Console.WriteLine(StringValue);
