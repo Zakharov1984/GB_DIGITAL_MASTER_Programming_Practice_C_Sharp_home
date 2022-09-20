5a6d181f226e74d47d7cca8a4d707773ad45887b
Zakharov <zakharov.products@gmail.com>
2022-09-20 04:33:14 +0300

    First_task_completed_in_Part_2

---
+++ Part_2/Task_0/Program.cs
@@ -0,0 +1,7 @@
+﻿void SecondDigitOf(int number) {
+    Console.WriteLine( ( (number % 100) - (number % 10) )  / 10 );
+}
+
+Console.Write("ПОжалуйста, введите трехзначное число: ");
+int number = Convert.ToInt32( Console.ReadLine() );
+SecondDigitOf(number);
