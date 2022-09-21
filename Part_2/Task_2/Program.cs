57f5d759a416b4001451ddc7efaea69feae4996e
Zakharov <zakharov.products@gmail.com>
2022-09-21 03:51:44 +0300

    Third_task_completed_in_Part_2

---
+++ Part_2/Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Пожалуйста, введите значение: ");
+int dayNumber = Convert.ToInt32( Console.ReadLine() );
+
+while (dayNumber < 1 || dayNumber > 7) {
+    Console.Clear();
+    Console.WriteLine("Пожалуйста введите актуальоне занчение, на этой планете семь дней в неделе:)");
+    Console.Write("Пожалуйста, введите значение: ");
+    dayNumber = Convert.ToInt32( Console.ReadLine() );
+}
+
+if (dayNumber == 6 || dayNumber == 7) {
+    Console.WriteLine("ДА, сегодня выходной день");
+}else {
+    Console.WriteLine("Сегодня рабочий день.");
+}
