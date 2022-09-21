bc489a82c37742af906e80f63a1ed974f3a5fdd2
Zakharov <zakharov.products@gmail.com>
2022-09-21 03:26:18 +0300

    Second_task_completed_in_Part_2

---
+++ Part_2/Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("ПОжалуйста, введите число: ");
+int number = Convert.ToInt32( Console.ReadLine() );
+
+while (number < 100) {
+    Console.WriteLine("ПОжалуйста, введите значение, которое больше или равно 100, в этом числе нет третьей цифры!");
+    Console.Write("ПОжалуйста, введите число: ");
+    number = Convert.ToInt32( Console.ReadLine() );
+}
+
+Console.WriteLine( (number / 100) % 10 );
+
