fc74fa4fa56026ad7072a1590e163cdbd79a2777
Zakharov <zakharov.products@gmail.com>
2022-09-25 06:20:28 +0300

    Third_task_completed_in_Part_3

---
+++ Part_3/Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Пожалуйста, введите число: ");
+int numberFromUser = Convert.ToInt32( Console.ReadLine() );
+string rowOfCubesOfNumbers = "";
+
+for (int i = 1; i <= numberFromUser; i++) {
+
+    if (i == numberFromUser) {
+        rowOfCubesOfNumbers += i * i * i;
+        break;
+    }
+
+    rowOfCubesOfNumbers += i * i * i + ", ";
+}
+
+Console.WriteLine($"Таблица кубов чисел от 1 до {numberFromUser}:  {rowOfCubesOfNumbers}");
+
