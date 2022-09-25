ec6d5546862811b995d034bc1c331ce03677f309
Zakharov <zakharov.products@gmail.com>
2022-09-25 04:49:10 +0300

    First_task_completed_in_Part_3

---
+++ Part_3/Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.Write("Пожалуйста, введите пятизначное значение: ");
+int numberFromUser = Convert.ToInt32( Console.ReadLine() );
+int changedValue = numberFromUser;
+string reverseNumber = "";
+
+while (numberFromUser >= 100000 || numberFromUser < 10000) {
+    Console.Clear();
+    Console.WriteLine("Пожалуйста введите корректное число.");
+    Console.Write("Пожалуйста, введите пятизначное значение: ");
+    numberFromUser = Convert.ToInt32( Console.ReadLine() );
+    changedValue = numberFromUser;
+}
+
+for (int count = 0; count < 5; count++) { // or 0 < changedValue
+    reverseNumber += changedValue % 10;
+    changedValue /= 10;
+}
+
+if ( numberFromUser == int.Parse(reverseNumber) ) {
+    Console.WriteLine($"число {numberFromUser} - является палиндромом");
+}else {
+    Console.WriteLine($"число {numberFromUser} - не является палиндромом");
+}
+
+
