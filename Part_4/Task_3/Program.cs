9cc83222fa0e4f9bb0132e92297ef7b317d49637
Zakharov <zakharov.products@gmail.com>
2022-09-29 06:42:33 +0300

    Third_task_completed_in_Part_4

---
+++ Part_4/Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] GetArrayOfNumbers(int numberOfElementsInArray) {
+    int[] newArray = new int[numberOfElementsInArray];
+
+    for (int i = 0; i < numberOfElementsInArray; i++) {
+        newArray[i] = new Random().Next(0, 99 + 1);
+    }
+
+    printArray(newArray);
+    return newArray;
+}
+
+
+void printArray(int[] array) {
+    string totalStringOfelementsOfarray = "[ ";
+    for (int i = 0; i < array.Length - 1; i++){
+        totalStringOfelementsOfarray += $"{array[i]}, ";
+    }
+
+    totalStringOfelementsOfarray += $"{totalStringOfelementsOfarray.Length - 1} ]";
+
+    Console.WriteLine($"Массив из {array.Length}: {totalStringOfelementsOfarray}");
+}
+
+GetArrayOfNumbers(20);
