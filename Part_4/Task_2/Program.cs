4b426ba906af86d45b054f3c29798474da3d4c5b
Zakharov <zakharov.products@gmail.com>
2022-09-29 04:45:55 +0300

    Second_task_completed_in_Part_4

---
+++ Part_4/Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetSumDigits(int number) {
+    int totalValue = 0;
+
+    while (number > 0) {
+        totalValue += number % 10;
+        number /= 10;
+    }
+
+    Console.WriteLine(totalValue);
+    return totalValue;
+}
+
+GetSumDigits(555); // => 15
+GetSumDigits(123); // => 6
+GetSumDigits(123456789); // => 45
