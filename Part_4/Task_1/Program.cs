db6f3e577e3f008fea82bb30ad064a922d4244f7
Zakharov <zakharov.products@gmail.com>
2022-09-29 04:28:08 +0300

    First_task_completed_in_Part_4

---
+++ Part_4/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿int getNumberToDegree(int number, int degree) {
+    int totalNumber = 1;
+    while (degree > 0) {
+        totalNumber *= number;
+        degree--;
+    }
+
+    Console.WriteLine(totalNumber);
+    return totalNumber;
+}
+
+getNumberToDegree(3, 3); // => 27
+getNumberToDegree(5, 3); // =>  125
+getNumberToDegree(5, 0); // => 1
