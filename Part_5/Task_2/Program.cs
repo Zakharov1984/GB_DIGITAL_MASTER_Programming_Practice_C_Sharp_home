c0749f50d797c52021dc147045fab699cee6e4f7
Zakharov <zakharov.products@gmail.com>
2022-10-02 10:44:07 +0300

    second_task_completed_in_Part_5

---
+++ Part_5/Task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿void printArray(int[] array) {
+    string totalStringOfelementsOfarray = "[ ";
+    for (int i = 0; i < array.Length -1 ; i++){
+        totalStringOfelementsOfarray += $"{array[i]}, ";
+    }
+
+    totalStringOfelementsOfarray += $"{array[array.Length - 1]} ]";
+
+    Console.WriteLine($"Массив из {array.Length}: {totalStringOfelementsOfarray}");
+}
+
+void sumOfOddElements(int[] array) {
+    int totalValue = 0;
+
+    for (int i = 1; i < array.Length; i++) {
+        totalValue += i % 2 == 1 ? array[i] : 0;
+    }
+
+    Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях = {totalValue}");
+}
+
+
+int[] newArray(int amountOfElements) {
+    int[] newArray = new int[amountOfElements];
+    for (int i = 0; i < amountOfElements; i++) {
+        newArray[i] = new Random().Next(-99, 99 + 1);
+    }
+    Console.Clear();
+    printArray(newArray); 
+    sumOfOddElements(newArray);
+    return newArray;
+}
+
+
+
+
+newArray(10);
