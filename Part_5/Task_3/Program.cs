91e076a4e72c8986a980181925f01e3c6b9f9ee6
Zakharov <zakharov.products@gmail.com>
2022-10-03 04:29:56 +0300

    third_task_completed_in_Part_5

---
+++ Part_5/Task_3/Program.cs
@@ -0,0 +1,43 @@
+﻿void printArray(double[] array) {
+    string totalStringOfelementsOfarray = "[ ";
+    for (int i = 0; i < array.Length -1 ; i++){
+        totalStringOfelementsOfarray += $"{array[i]}, ";
+    }
+
+    totalStringOfelementsOfarray += $"{array[array.Length - 1]} ]";
+
+    Console.WriteLine($"Массив из {array.Length}: {totalStringOfelementsOfarray}");
+}
+
+
+
+
+void DifferenceMaxMinElements(double[] array) {
+    double  minValue = array[0],
+            maxValue = array[0];
+
+    for (int i = 1; i < array.Length; i++) {
+        if (minValue > array[i]) minValue = array[i];
+        if (maxValue < array[i]) maxValue = array[i];
+    }
+
+    Console.Clear();
+    printArray(array);
+    Console.WriteLine($"Разница между минимальным значением {minValue} и масимальным значением {maxValue} = {Math.Round(maxValue - minValue, 1)}");
+} 
+
+
+double[] newArray(int amountOfElements) {
+    double[] newArray = new double[amountOfElements];
+    Random randomObj = new Random();
+    for (int i = 0; i < amountOfElements; i++) {
+        newArray[i] = Math.Round(randomObj.Next(-99, 99 + 1) + randomObj.NextDouble(), 1)  ;
+    } 
+    return newArray;
+}
+
+
+
+
+
+DifferenceMaxMinElements( newArray(10) );
