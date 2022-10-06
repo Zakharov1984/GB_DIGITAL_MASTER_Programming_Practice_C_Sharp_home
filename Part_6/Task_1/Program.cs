f8252b94f8a9326120870acceeb6ea3c8ce8be63
Zakharov <zakharov.products@gmail.com>
2022-10-06 09:08:53 +0300

    first_task_completed_in_Part_6

---
+++ Part_6/Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("ПОжалуйста, введитче набор чисел через пробел.");
+
+string[] arrayValues = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+positiveNumberCounter(arrayValues);
+
+void positiveNumberCounter(string[] array) {
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++) {
+        if (int.Parse(array[i]) > 0) counter++;
+    }
+    Console.WriteLine($"Положительных чисел: {counter}");
+}
+
+
