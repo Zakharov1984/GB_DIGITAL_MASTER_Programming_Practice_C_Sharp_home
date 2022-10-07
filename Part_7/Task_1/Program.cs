615fc11e430bba61157b719dda56335748e67a7a
Zakharov <zakharov.products@gmail.com>
2022-10-07 08:19:50 +0300

    first_task_completed_in_Part_7

---
+++ Part_7/Task_1/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+Console.Write("ПОжалуйста, введите число строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("ПОжалуйста, введите число столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+
+GetDimensionalArrayOfRealNumbers(rows, columns);
+
+
+
+
+
+
+double [,] GetDimensionalArrayOfRealNumbers (int rows, int columns) {
+    double[,] newDimensionalArray = new double[rows,columns];
+    Random randomObj = new Random();
+    for (int i = 0; i < newDimensionalArray.GetLength(0); i++) {
+        for (int j = 0; j < newDimensionalArray.GetLength(1); j++) {
+            newDimensionalArray[i,j] = Math.Round(randomObj.Next(-99, 99 + 1) + randomObj.NextDouble(), 1);
+        }    
+    }
+    multidimensionalArrayPrint(newDimensionalArray);
+    return newDimensionalArray;
+} 
+
+
+
+
+
+
+void multidimensionalArrayPrint(double[,] array) {
+    string totalString = "[ \n";
+
+    for (int i = 0; i < array.GetLength(0); i++) {
+        totalString += "[ ";
+        for (int j = 0; j < array.GetLength(1); j++) {
+            totalString += array[i,j] + " ";
+        }
+        totalString += "],\n";
+    }
+
+    totalString += "]";
+
+    Console.WriteLine(totalString);
+}
