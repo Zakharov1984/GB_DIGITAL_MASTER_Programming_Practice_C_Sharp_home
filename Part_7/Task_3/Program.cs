5d4a3f33016c750bcc08e7c6afb0604d7e1cb990
Zakharov <zakharov.products@gmail.com>
2022-10-10 17:03:31 +0300

    third_task_completed_in_Part_7

---
+++ Part_7/Task_3/Program.cs
@@ -0,0 +1,46 @@
+﻿
+AverageColumns(GetDimensionalArrayOfNumbers(3, 3));
+
+
+void AverageColumns(int[,] array) {
+    string totalString = "Среднее арифметическое каждого столбца: ";
+    for (int i = 0; i < array.GetLength(1); i++) {
+        double averageOneColumns = 0;
+        for (int j = 0; j < array.GetLength(0); j++) {
+            averageOneColumns += array[j, i];
+        }
+        totalString += $"{Math.Round(averageOneColumns / array.GetLength(0), 1)}; ";  
+    }
+
+    Console.WriteLine(totalString);
+}
+
+
+int [,] GetDimensionalArrayOfNumbers (int rows, int columns) {
+    int[,] newDimensionalArray = new int[rows,columns];
+    Random randomObj = new Random();
+    for (int i = 0; i < newDimensionalArray.GetLength(0); i++) {
+        for (int j = 0; j < newDimensionalArray.GetLength(1); j++) {
+            newDimensionalArray[i,j] = randomObj.Next(1, 9 + 1);
+        }    
+    }
+    multidimensionalArrayPrint(newDimensionalArray);
+    return newDimensionalArray;
+}
+
+
+void multidimensionalArrayPrint(int[,] array) {
+    string totalString = "[ \n";
+
+    for (int i = 0; i < array.GetLength(0); i++) {
+        totalString += "[ ";
+        for (int j = 0; j < array.GetLength(1); j++) {
+            totalString += array[i,j] + " ";
+        }
+        totalString += "],\n";
+    }
+
+    totalString += "]";
+
+    Console.WriteLine(totalString);
+}
