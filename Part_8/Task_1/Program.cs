3c428ef227c33edf47c5b9b17061fc6f158dc630
Alexandr Zakharov <zakharov.products@gmail.com>
2022-10-13 15:04:23 +0300

    first_task_completed_in_Part_8

---
+++ Part_8/Task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿int[,] newArray = new int[,] {{1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}};
+arrangeItems(newArray);
+
+void arrangeItems(int[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1) - 1; j++){
+            for (int k = 0 ; k < array.GetLength(1) - 1 - j; k++) {
+                if (array[i,k] < array[i,k + 1]) {
+                    int temp = array[i,k];
+                    array[i,k] = array[i,k + 1];
+                    array[i,k + 1] = temp; 
+                }
+            }
+        }
+    }
+    multidimensionalArrayPrint(array);
+}
+
+void multidimensionalArrayPrint(int[,] array) {
+    string totalString = "[ \n";
+
+    for (int i = 0; i < array.GetLength(0); i++) {
+        totalString += "[ ";
+        for (int j = 0; j < array.GetLength(1); j++) {
+            totalString += array[i,j] + " ";
+        }
+        totalString += "],\n";
+    }
+
+    totalString += "]";
+
+    Console.WriteLine(totalString);
+}
