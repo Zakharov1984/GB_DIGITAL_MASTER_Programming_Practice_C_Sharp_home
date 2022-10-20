3de079924e4450f4904c13cb00b8b7281ee71b4f
Alexandr Zakharov <zakharov.products@gmail.com>
2022-10-20 06:13:19 +0300

    first_task_completed_in_Part_9

---
+++ Part_9/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int finishNumber = Convert.ToInt32(Console.ReadLine());
+string totalString = "";
+
+void printNaturalsNumbers(int num) {
+    if (num != 0){
+        totalString += num + " ";
+        printNaturalsNumbers(num - 1);
+    }
+    else Console.WriteLine(totalString);
+}
+
+printNaturalsNumbers(finishNumber);
