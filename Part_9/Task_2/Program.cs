1f3970bc8358d2e5f7941200b7bed3739a8fbefd
Alexandr Zakharov <zakharov.products@gmail.com>
2022-10-20 06:48:40 +0300

    second_task_completed_in_Part_9

---
+++ Part_9/Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите начальоне число: ");
+int startNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конечное число: ");
+int finishNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetSumNaturalsNumbersInInterim(startNumber, finishNumber));
+
+int GetSumNaturalsNumbersInInterim(int m, int n) {
+    if (m == n) return m;
+    else return n + GetSumNaturalsNumbersInInterim(m, n - 1);
+}
+
+
